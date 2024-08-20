b0ee8f95e07af6cccad7a6c695b273bf9677deff
Jairo Rincon <rincon303@gmail.com>
2024-08-19 23:01:08 -0500

    Crud User terminado

---
+++ src/models/User.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+const bcrypt = require('bcrypt')
+
+const User = sequelize.define('user', {
+    
+        firstName: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        lastName: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        phone: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          unique: true
+        },
+});
+
+User.beforeCreate(async(User)=>{
+    const Password = User.password;
+    const hashPassword = await bcrypt.hash(Password, 10);
+    User.password = hashPassword;
+
+})
+
+module.exports = User;
